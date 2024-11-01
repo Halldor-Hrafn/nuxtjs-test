e496cf5e07dbec98e164afd39be4e138431e07c4
Halldór Hrafn Reynisson <halldor.hrafn.reynisson@gmail.com>
2024-11-01 15:38:55 +0000

    stuff

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/hello')
+</script>
+
+<template>
+  <div>
+    <h1>Index Page</h1>
+    
+    <p>{{ data }}</p>
+  </div>
+</template>
